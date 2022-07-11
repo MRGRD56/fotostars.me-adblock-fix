3bf7c4bc786a3274b062eea996f39d1e6220d4bb
KIRILL GOLIKOV <35491968+MRGRD56@users.noreply.github.com>
2022-07-11 22:38:16 +0500

    Update and rename index.js to index.user.js

--- index.user.js
+++ index.user.js
@@ -4,13 +4,9 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       MRGRD56
-// @match        https://fotostars.me/
+// @match        https://fotostars.me/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    googletag.pubads = () => ({refresh: () => {}});
-})();
+googletag.pubads = () => ({refresh: () => {}});
