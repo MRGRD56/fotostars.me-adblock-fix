80d4dd190d8077c651584ef3adba2c2035ea6289
KIRILL GOLIKOV <35491968+MRGRD56@users.noreply.github.com>
2022-07-11 22:34:09 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         fotostart.me adblock fix
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       MRGRD56
+// @match        https://fotostars.me/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    googletag.pubads = () => ({refresh: () => {}});
+})();
