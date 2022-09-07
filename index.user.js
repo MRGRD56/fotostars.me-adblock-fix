6f15b32474c298b616d99c525d8284a9058d9a8c
KIRILL GOLIKOV <35491968+MRGRD56@users.noreply.github.com>
2022-09-07 19:13:42 +0500

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -2,7 +2,6 @@
 // @name         fotostart.me adblock fix
 // @namespace    https://github.com/MRGRD56
 // @version      0.1
-// @description  try to take over the world!
 // @author       MRGRD56
 // @match        https://fotostars.me/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
